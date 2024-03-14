94a33e4f1782a29f21caaf1f982f712ba0f5de5b
Lsael <l.saelens@proton.me>
2024-03-14 12:45:06 +0100

    Added edit name modal

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,6 +4,14 @@ import { Navigate } from "react-router-dom";
 const Profil = () => {
   const user = useSelector((state) => state.user)
 
+  const handleOpenModal = () => {
+    document.querySelector("#edit-modal").showModal()
+  }
+
+  const handleCloseModal = () => {
+    document.querySelector("#edit-modal").close()
+  }
+
   if(!user.token) {
     return <Navigate to="/login" />;
   } else {
@@ -11,7 +19,17 @@ const Profil = () => {
         <main className="main bg-dark">
         <div className="header">
           <h1 id="profile-username">Welcome back<br />{`${user.userDatas.firstName} ${user.userDatas.lastName}`}</h1>
-          <button className="edit-button">Edit Name</button>
+          <button className="edit-button" onClick={handleOpenModal}>Edit Name</button>
+          <dialog id="edit-modal">
+            <i className="fa fa-user-circle sign-in-icon"></i>
+            <span id="close-modal-button" onClick={handleCloseModal}>&#10006;</span>
+            <span></span>
+            <span htmlFor="firstName-input">New firstname</span>
+            <input name="firstName-input" type={"text"} defaultValue={user.userDatas.firstName}></input>
+            <span htmlFor="lastName-input">New lastname</span>
+            <input name="lastName-input" type={"text"} defaultValue={user.userDatas.lastName}></input>
+            <button type="submit">Validate</button>
+          </dialog>
         </div>
         <h2 className="sr-only">Accounts</h2>
         <section className="account">
